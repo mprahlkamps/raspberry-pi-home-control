5ccda9b7ace75738ba85a13d91614696c86f8eb4
Maximilian Prahl-Kamps <m.prahl-kamps@live.de>
2017-03-01 02:28:46 +0100

    Adding initial readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# raspberry-pi-home-control
+# Raspberry Pi Home Control
+
+## Protocol
+
+RPHC uses a JSON message protocol.
+ 
+ ### Connection
+ 
+ When a client connects to the server, the server sends a welcome message alongside
+ useful information to setup the client.
+ 
+ ``` json
+ {
+   "type": "welcome",
+   "available-modules": []
+ }
+ ```
+ * **type** - the type of the message
+ * **available-modules** - an array representing available modules on this server and
+    information about them
--- src/main/java/de/pk/rphc/server/ControlServer.java
+++ src/main/java/de/pk/rphc/server/ControlServer.java
@@ -9,26 +9,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ControlServer extends WebSocketServer {
-	
+
 	private Logger logger;
 
 	public ControlServer(InetSocketAddress address) {
 		super(address);
 
 		logger = LoggerFactory.getLogger(ControlServer.class);
-	    logger.info("Staring Control Server (" + address + ")");
+		logger.info("Staring Control Server (" + address + ")");
 	}
 
 	@Override
 	public void onMessage(WebSocket connection, String message) {
 		logger.debug("(" + connection.getRemoteSocketAddress() + ") New message: " + message);
 	}
-	
+
 	@Override
 	public void onOpen(WebSocket connection, ClientHandshake handshake) {
 		logger.info("(" + connection.getRemoteSocketAddress() + ") New Connection");
 	}
-	
+
 	@Override
 	public void onClose(WebSocket connection, int code, String reason, boolean remote) {
 		logger.info("(" + connection.getRemoteSocketAddress() + ") Connection closed");

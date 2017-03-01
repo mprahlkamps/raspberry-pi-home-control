d0ef05f2d7d403021367c73392fa7a15ce3d44c1
Maximilian Prahl-Kamps <m.prahl-kamps@live.de>
2017-03-01 01:57:39 +0100

    added initial files

---
+++ src/main/java/de/pk/rphc/App.java
@@ -0,0 +1,16 @@
+package de.pk.rphc;
+
+import de.pk.rphc.server.ControlServer;
+
+import java.net.InetSocketAddress;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ControlServer server = new ControlServer(new InetSocketAddress(9999));
+        server.start();
+
+    }
+
+}
---
+++ src/main/java/de/pk/rphc/server/ControlServer.java
@@ -0,0 +1,42 @@
+package de.pk.rphc.server;
+
+import java.net.InetSocketAddress;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ControlServer extends WebSocketServer {
+	
+	private Logger logger;
+
+	public ControlServer(InetSocketAddress address) {
+		super(address);
+
+		logger = LoggerFactory.getLogger(ControlServer.class);
+	    logger.info("Staring Control Server (" + address + ")");
+	}
+
+	@Override
+	public void onMessage(WebSocket connection, String message) {
+		logger.debug("(" + connection.getRemoteSocketAddress() + ") New message: " + message);
+	}
+	
+	@Override
+	public void onOpen(WebSocket connection, ClientHandshake handshake) {
+		logger.info("(" + connection.getRemoteSocketAddress() + ") New Connection");
+	}
+	
+	@Override
+	public void onClose(WebSocket connection, int code, String reason, boolean remote) {
+		logger.info("(" + connection.getRemoteSocketAddress() + ") Connection closed");
+	}
+
+	@Override
+	public void onError(WebSocket connection, Exception exception) {
+		logger.error("(" + connection.getRemoteSocketAddress() + ") Error", exception);
+	}
+
+}
